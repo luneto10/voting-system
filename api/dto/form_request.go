adf43736c1242f7dab3cd2fe6ed7e5df87cd00e7
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2025-05-18 20:19:36 -0300

    feat: add DTOs for form requests and responses with validation

---
+++ api/dto/form_request.go
@@ -0,0 +1,28 @@
+package dto
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+type CreateFormRequest struct {
+	Title string `json:"title" validate:"required,min=5,max=100"`
+}
+
+func (r *CreateFormRequest) Validate() error {
+	validate := validator.New()
+	return validate.Struct(r)
+}
+
+type UpdateFormRequest struct {
+	Title string `json:"title" validate:"omitempty,min=5,max=100"`
+}
+
+func (r *UpdateFormRequest) Validate() error {
+	validate := validator.New()
+	return validate.Struct(r)
+}
+
+type GetFormResponse struct {
+	ID    uint `json:"id"`
+	Title string `json:"title"`
+}
