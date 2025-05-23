64397dbb83d50d835acb872e39aacc1b6da4769e
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2025-05-23 19:39:10 -0300

    chore: update dependencies in go.mod and go.sum for improved functionality and security, including CORS support in router

--- Server/api/router/router.go
+++ Server/api/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"gorm.io/gorm"
@@ -8,6 +9,14 @@ import (
 
 func Initialize(db *gorm.DB) {
 	router := gin.Default()
+
+	// Configure CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}))
 
 	handlers := initDependencies(db)
 
